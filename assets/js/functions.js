ec6c971f0ca28e56877ef7783834f9bba26713a1
Manuel <strombolys@gmail.com>
2017-05-12 17:36:09 +0100

    js staff
    JS and JsQuery works!

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,38 @@
+(function($){
+		var $mobileNavToggleBtn,
+			$blankATags	= $('a[href^="#"]'),
+			$body = $('html, body'),
+			settings = {
+				duration: 1500
+			};
+
+		function onBtnClick	(e){
+			var $this = $(this);
+			var $selectors = $('.mobile-nav');
+			$this.toggleClass('is-open');	
+			$selectors.toggleClass('is-open');
+		}
+
+		function onBlankAClick(event){
+			var $this = $(this),
+				href = $this.attr('href'),
+				$target = $(href);
+
+			if($target.length > 0){
+				event.preventDefault();
+				$body.animate({
+					scrollTop: $target.offset().top
+				},settings.duration);
+			}
+		}
+
+		$(document).ready(function(){
+			$mobileNavToggleBtn	= $('.mobile-nav-toggle');
+			$mobileNavToggleBtn.on('click', onBtnClick);
+			$blankATags.on('click', onBlankAClick);
+		});
+
+})(jQuery);
 /*
 $('.js-toggle-menu').click(function(e){
   e.preventDefault();
